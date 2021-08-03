537648ab349636742a2a00d520b3b6f779ef6cab
nima-kam <nimakambarani007@gmail.com>
2021-08-03 22:16:32 +0430

    Initial adding of the pychrm Flask project

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    def get(self):
+
+        return {'hello': 'world'}
+
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=1)
+
