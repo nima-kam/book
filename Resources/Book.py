422604ccbe5e1088b49686585de1009e8303b665
Nima <56191102+nima-kam@users.noreply.github.com>
2021-08-07 17:35:25 +0430

    book files added

---
+++ Resources/Book.py
@@ -0,0 +1,9 @@
+from flask_restful import Resource, reqparse
+from flask_jwt_extended import (
+    jwt_required,
+    get_jwt_claims,
+    get_jwt_identity,
+    jwt_optional,
+    fresh_jwt_required,
+)
+from Models.Book import BookModel
--- app.py
+++ app.py
@@ -13,6 +13,17 @@ class HelloWorld(Resource):
 
 api.add_resource(HelloWorld, '/')
 
+# api.add_resource(Store, "/store/<string:name>")
+# api.add_resource(StoreList, "/stores")
+api.add_resource(Book, "/book/<string:name>")
+api.add_resource(BookList, "/books")
+api.add_resource(UserRegister, "/register")
+api.add_resource(User, "/user/<int:user_id>")
+api.add_resource(UserLogin, "/login")
+api.add_resource(TokenRefresh, "/refresh")
+api.add_resource(UserLogout, "/logout")
+
+
 if __name__ == '__main__':
     app.run(debug=1)
 
